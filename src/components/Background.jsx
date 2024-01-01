fccb5a5f8e45651d525c71bb361eaa154b12c362
JiaLee0707 <easyah0707@gmail.com>
2024-01-01 23:58:55 +0900

    [Feat] mousemove snow move

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -12,8 +12,6 @@ const Background = () => {
     const [snows, setSnows] = useState([]);
     const [snowLine, setSnowLine] = useState(0);
     const [snowLineRandom, setSnowLineRandom] = useState(false);
-    // const [deleteSnows, setDeleteSnows] = useState([]);
-    const [deleteSnow, setDeleteSnow] = useState(null);
 
     const snowContainerRef = useRef(null);
 
@@ -76,154 +74,6 @@ const Background = () => {
         }
     }, [snows]);
 
-    // const handleMouseMove = useCallback(() => {
-    //     console.log('deleteSnows', deleteSnows);
-    //     for (let del of deleteSnows) {
-    //         console.log('del', del);
-    //         const absLeft = Math.abs(del.left);
-    //         const tolerance = 1;
-    //         const updatedComponents = snows.map((snow) => {
-    //             console.log('snow.left', snow.props.left);
-    //             const absNumber = Math.abs(snow.props.left);
-    //             const withinRange = absLeft >= absNumber - tolerance && absLeft <= absNumber + tolerance;;
-    //             if (withinRange && snow.props.line > del.line) {
-    //                 console.log('if', {
-    //                     ...snow,
-    //                     props: {
-    //                         ...snow.props,
-    //                         line: snow.line - 1 * SNOW_MAX_SIZE / 2
-    //                     }
-    //                 })
-    //                 return {
-    //                     ...snow,
-    //                     props: {
-    //                         ...snow.props,
-    //                         line: snow.line - 1 * SNOW_MAX_SIZE / 2
-    //                     }
-    //                 };
-    //             }
-    //             console.log('snow', snow);
-    //             return snow;
-    //         });
-    //         console.log('updatedComponents', updatedComponents);
-    //     }
-    // }, [deleteSnows]);
-
-    useEffect(() => {
-        // handleMouseMove 함수 정의
-        const handleMouseMove = () => {
-            // console.log('deleteSnows', deleteSnows);
-            // for (let del of deleteSnows) {
-            //     // console.log('del', del);
-            //     const absLeft = Math.abs(del.left);
-            //     const tolerance = 1;
-            //     setSnows(prevSnows => prevSnows.map((snow) => {
-            //         // console.log('snow.left', snow.left);
-            //         const absNumber = Math.abs(snow.left);
-            //         const withinRange = absLeft >= absNumber - tolerance && absLeft <= absNumber + tolerance;;
-            //         if (withinRange && snow.line > del.line) {
-            //             // console.log('if', {
-            //             //     ...snow,
-            //             //     line: snow.line - 1 * SNOW_MAX_SIZE / 2
-            //             // })
-            //             return {
-            //                 ...snow,
-            //                 line: snow.line - 1 * SNOW_MAX_SIZE / 2
-            //             };
-            //         }
-            //         // console.log('snow', snow);
-            //         return snow;
-            //     }));
-            // }
-
-            // setDeleteSnows(prevSnows => {
-            //     const updatedSnows = [];
-            //     // console.log('aa', prevSnows);
-            //     prevSnows.forEach((del) => {
-            //         const absLeft = Math.abs(del.left);
-            //         const tolerance = 1;
-            //         // const updatedComponents = snows.map((snow) => {
-            //         //     const absNumber = Math.abs(snow.left);
-            //         //     console.log('absLeft', absLeft);
-            //         //     console.log('absNumber', absNumber);
-            //         //     const withinRange = absLeft >= absNumber - tolerance || absLeft <= absNumber + tolerance;
-            //         //     if (withinRange && snow.line > del.line) {
-            //         //         console.log('if');
-            //         //         return {
-            //         //             ...snow,
-            //         //             line: snow.line - 1 * SNOW_MAX_SIZE / 2
-            //         //         };
-            //         //     }
-            //         //     return snow;
-            //         // });
-            //         // for (let snow of snows) {
-            //         snows.forEach((snow, index) => {
-            //             const tempSnows = [...snows];
-            //             const absNumber = Math.abs(snow.left);
-            //             console.log('absLeft', absLeft);
-            //             console.log('absNumber', absNumber);
-            //             const withinRange = absLeft >= absNumber - tolerance || absLeft <= absNumber + tolerance;
-            //             if (withinRange && snow.line > del.line) {
-            //                 console.log('if');
-            //                 tempSnows[index] = {
-            //                     ...snow,
-            //                     line: snow.line - 1 * SNOW_MAX_SIZE / 2
-            //                 };
-            //                 setSnows(tempSnows);
-            //             }
-            //         });
-            //         // console.log('updatedComponents.length', updatedComponents.length);
-            //         // console.log('snows.legnth11', snows.length);
-            //         // setSnows(updatedComponents);
-            //         // console.log('snows.legnth22', snows.length);
-            //
-            //         // 여기서 필요한 로직을 수행하고 updatedComponents로 업데이트된 상태를 사용하여 return 하거나
-            //         // 필요에 따라 상태를 직접 업데이트할 수 있습니다.
-            //         // 예시: updatedSnows.push({ ...del, line: updatedLine });
-            //     });
-            //     // 업데이트가 필요한 경우 상태를 직접 변경하거나 리턴합니다.
-            //     return updatedSnows; // 상태를 직접 변경할 때는 필요에 따라 리턴
-            // });
-
-
-            // console.log('del', deleteSnow);
-
-            const absLeft = Math.abs(deleteSnow?.left);
-            const tolerance = 0.5;
-            setSnows(prevSnows => prevSnows?.map((snow) => {
-                // console.log('snow', snow.key);
-                // console.log('deleteSnow', deleteSnow);
-                // console.log('deleteSnow.left', deleteSnow?.left);
-                if (snow?.left === deleteSnow?.left && snow?.line === deleteSnow?.line) return;
-                const absNumber = Math.abs(snow?.left);
-                const withinRange = absLeft >= absNumber - tolerance || absLeft <= absNumber + tolerance;
-                if (withinRange && snow?.line > deleteSnow?.line) {
-                    console.log('if', {
-                        ...snow,
-                        line: snow?.line - 1 * SNOW_MAX_SIZE / 2
-                    })
-                    const newLine = snow?.line - 1 * SNOW_MAX_SIZE / 2;
-                    setSnowLine(prevSnowLine => prevSnowLine - 1);
-                    return {
-                        ...snow,
-                        line: newLine
-                    };
-                }
-                // console.log('snow', snow);
-                return snow;
-            }));
-
-        };
-
-        window.addEventListener('mousemove', handleMouseMove);
-
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-        };
-    }, [deleteSnow]); // deleteSnows가 업데이트될 때마다 useEffect 재실행
-
-
-
     return (
         <Container ref={snowContainerRef}>
             {/*{snows?.map((snow) => snow)}*/}
@@ -233,13 +83,13 @@ const Background = () => {
                     left={snow?.left}
                     size={snow?.size}
                     line={snow?.line}
-                    isMouseOver={() => {
-                        const left = snow?.left;
-                        const line = snow?.line;
-                        // setDeleteSnow(prevSnows => [...prevSnows, { left, line }]);
-                        setDeleteSnow({ left, line });
-                        // console.log('del', deleteSnows);
-                    }}
+                    // isMouseOver={() => {
+                    //     const left = snow?.left;
+                    //     const line = snow?.line;
+                    //     // setDeleteSnow(prevSnows => [...prevSnows, { left, line }]);
+                    //     setDeleteSnow({ left, line });
+                    //     // console.log('del', deleteSnows);
+                    // }}
                 />
             ))}
         </Container>
--- src/components/Snow.jsx
+++ src/components/Snow.jsx
@@ -2,15 +2,13 @@ import React, {useEffect, useRef, useState} from 'react';
 import snowflake from "../assets/snowflake-solid.svg";
 import {SnowWrap} from "./Background.style";
 
-const Snow = ({ left, size, line, isMouseOver }) => {
+const Snow = ({ left, size, line }) => {
+    const [$left, setLeft] = useState(left);
     const snowRef = useRef(null);
     const [currentLine, setCurrentLine] = useState(0);
 
     useEffect(() => {
         if (snowRef.current) {
-            // 한 절반부터는 첫번째 라인 두번째 라인 랜덤
-            // const translateYValue = `calc(100vh - ${size}px * ${snows.length + 1} / ${Math.ceil(window.innerWidth / size)})`;
-            // const translateYValue = `calc(100vh - ${line}vh * ${line + 1} / ${Math.ceil(window.innerWidth / line)})`;
             const translateYValue = `calc(100vh - ${line}px)`;
             const snowAnimation = [
                 { transform: `translateY(${currentLine}) rotate(0deg)` }, // 시작 위치
@@ -32,24 +30,146 @@ const Snow = ({ left, size, line, isMouseOver }) => {
         }
     }, [line]);
 
-    // useEffect(() => {
-    //     const handleMouseMove = (e) => {
-    //         if (isDragging) {
-    //             // 클릭한 상태에서 이동할 때 컴포넌트 삭제 로직
-    //             onDelete(left, line); // 해당 위치에 있는 컴포넌트 삭제
-    //             setIsDragging(false); // 클릭 상태 해제
-    //         }
-    //     };
-    //
-    //     window.addEventListener('mousemove', handleMouseMove);
-    //
-    //     return () => {
-    //         window.removeEventListener('mousemove', handleMouseMove);
-    //     };
-    // }, [getInfo]);
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            const mouseX = Math.abs(event.clientX);
+            const mouseY = Math.abs(event.clientY);
+
+            if (snowRef.current) {
+                const snowRect = snowRef.current.getBoundingClientRect();
+
+
+                const absSnowX = Math.abs(snowRect.x);
+                const absSnowY = Math.abs(snowRect.y);
+                const tolerance = 10;
+                if (Math.abs(mouseX - absSnowX) <= tolerance) {
+                    console.log('adsfasdf');
+                    // if ()
+                    let newLeft = 0;
+                    if (mouseX > absSnowX) {
+                        newLeft = `${absSnowX - 10}px`
+                    } else {
+                        newLeft = `${absSnowX + 10}px`;
+                    }
+                    const snowAnimation = [
+                        // {transform: `translateX(${absSnowX}px`},
+                        { left: newLeft },
+                    ];
+
+                    snowRef.current.animate(
+                        snowAnimation,
+                        {
+                            duration: 500, // 애니메이션 지속 시간 (5초)
+                            iterations: 1, // 한 번만 실행
+                            fill: 'both', // 애니메이션이 끝난 후 상태를 유지합니다.
+                            // 애니메이션이 끝난 후 실행할 콜백 함수
+                            // onfinish: function() {
+                            //     // 애니메이션이 끝난 후에 처리할 작업을 여기에 작성하세요
+                            //     console.log('애니메이션이 종료되었습니다.');
+                            //     snowRef.current.style.left = `${absSnowX}px`;
+                            //     // 예시: 다른 동작 실행하기
+                            //     // someFunction();
+                            // }
+                        }
+                    )
+                    // return new Promise((resolve) => {
+                    //
+                    //     snowRef.current.animate(
+                    //         snowAnimation,
+                    //         {
+                    //             duration: 100, // 애니메이션 지속 시간 (5초)
+                    //             iterations: 1, // 한 번만 실행
+                    //             fill: 'both', // 애니메이션이 끝난 후 상태를 유지합니다.
+                    //             // 애니메이션이 끝난 후 실행할 콜백 함수
+                    //             // onfinish: function() {
+                    //             //     // 애니메이션이 끝난 후에 처리할 작업을 여기에 작성하세요
+                    //             //     console.log('애니메이션이 종료되었습니다.');
+                    //             //     snowRef.current.style.left = `${absSnowX}px`;
+                    //             //     // 예시: 다른 동작 실행하기
+                    //             //     // someFunction();
+                    //             // }
+                    //         }
+                    //     ).addEventListener('finish', () => {
+                    //         // 애니메이션이 끝나면 resolve를 호출하여 Promise를 완료합니다.
+                    //         console.log('애니메이션이 종료되었습니다.');
+                    //         snowRef.current.style.left = `${absSnowX}px`;
+                    //         resolve();
+                    //     });
+                    // });
+                }
+                // snowRef.current.style.transform = `translate(${snowRef.current.x}px, ${snowRef.current.y}px)`;
+
+                // requestAnimationFrame(animateSnow);
+                // const snowRect = snowRef.current.getBoundingClientRect();
+                // console.log('snowRect', snowRect);
+                // const absSnowX = Math.abs(snowRect.x);
+                // const absSnowY = Math.abs(snowRect.y);
+                // const tolerance = 1;
+                // const withinRangeX = mouseX >= absSnowX - tolerance || mouseX <= absSnowX + tolerance;
+                // const withinRangeY = mouseY >= absSnowY - tolerance || mouseY <= absSnowY + tolerance;
+                // if (withinRangeX || withinRangeY) {
+                //     const snowAnimation = [
+                //         // { transform: `translate(${moveX}px, ${moveY}px)` },
+                //     ];
+                //     snowRef.current.animate(
+                //         snowAnimation,
+                //         {
+                //             duration: 1000, // 애니메이션 지속 시간 (5초)
+                //             // iterations: Infinity, // 무한 반복
+                //             easing: 'linear', // 애니메이션 속도 곡선
+                //             delay: Math.random() * 3000, // 랜덤한 딜레이 설정
+                //             // fill: "forwards",
+                //             // fill: 'both', // 애니메이션이 끝난 후 상태 유지
+                //         }
+                //     );
+                // }
+
+
+
+                // const snowX = snowRect.left + snowRect.width / 2;
+                // const snowY = snowRect.top + snowRect.height / 2;
+                //
+                // const deltaX = mouseX - snowX;
+                // const deltaY = mouseY - snowY;
+                // const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+                //
+                // const maxDistance = 100; // 마우스와 눈송이 간의 최대 거리 (조절 가능)
+                // const moveFactor = 5; // 눈송이의 이동 속도 (조절 가능)
+                //
+                // if (distance < maxDistance) {
+                //     const moveX = moveFactor * (deltaX / distance);
+                //     const moveY = moveFactor * (deltaY / distance);
+                //
+                //
+                //     const snowAnimation = [
+                //         { transform: `translate(${moveX}px, ${moveY}px)` }
+                //         // { transform: `translateY(100vh) rotate(360deg)` }, // 종료 위치
+                //     ];
+                //     snowRef.current.animate(
+                //         snowAnimation,
+                //         {
+                //             duration: 5000, // 애니메이션 지속 시간 (5초)
+                //             // iterations: Infinity, // 무한 반복
+                //             easing: 'linear', // 애니메이션 속도 곡선
+                //             delay: Math.random() * 3000, // 랜덤한 딜레이 설정
+                //             // fill: "forwards",
+                //             fill: 'both', // 애니메이션이 끝난 후 상태 유지
+                //         }
+                //     );
+                //     // snowRef.current.style.transform = `translate(${moveX}px, ${moveY}px)`;
+                // }
+            }
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
 
     return (
-        <SnowWrap ref={snowRef} $left={left} size={size} onMouseOver={isMouseOver}>
+        <SnowWrap ref={snowRef} $left={$left} size={size}>
             <img src={snowflake} alt="Snowflake" />
         </SnowWrap>
     );
